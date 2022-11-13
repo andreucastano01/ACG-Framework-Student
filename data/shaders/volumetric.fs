4987552d2fdb12bbbd892cc9f9ad7beee5266fb0
Andreu Castaño <andreu.castano01@estudiant.upf.edu>
2022-11-13 16:09:45 +0100

    Update volumetric.fs

--- data/shaders/volumetric.fs
+++ data/shaders/volumetric.fs
@@ -29,19 +29,20 @@ void main()
     ray_dir = normalize(ray_dir);
     //vec3 sample_pos = clamp(v_position, 0.0, 1.0);
     vec3 sample_pos = v_position;
+    vec3 samplepos01 = sample_pos * 0.5 + 0.5;
     vec3 stepLength = ray_dir * ray_step;
 
     //Sampleamos cada punto
     for(int i = 0; i < SAMPLES; i++){
-        float d = texture(u_texture, clamp(sample_pos, 0.0, 1.0)).x;
+        float d = texture(u_texture, samplepos01).x;
         vec4 sample_color = vec4(d,1-d,0,d*d);
         //Calculamos color
         final_color += stepLength * (1.0 - final_color.a) * sample_color;
         //Pasamos al siguiente sample
-        sample_pos += stepLength;
+        samplepos01 += stepLength;
         //Early termination
         if(final_color.a >= 1) break;
-        if(sample_pos.x > 1 || sample_pos.x < -1 || sample_pos.y > 1 || sample_pos.y < -1 || sample_pos.z > 1 || sample_pos.z < -1) break;
+        if(samplepos01.x > 1 || samplepos01.x < 0 || samplepos01.y > 1 || samplepos01.y < 0 || samplepos01.z > 1 || samplepos01.z < 0) break;
     }
     //Anades brightness
     //if (final_color.x <= 0.1 || final_color.y <= 0.1 || final_color.z <= 0.1) discard;
